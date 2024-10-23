56332a3046095b2628d1430c392890065ba19b95
andreyjgolub739 <andreyjgolub739@gmail.com>
2024-10-23 20:15:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const myName = "Андрій";
+let myAge = 21;
+let isStudent = true;
+
+console.log(typeof myName); // "string"
+console.log(typeof myAge); // "number"
+console.log(typeof isStudent); // "boolean"
+
+let person = {
+    name: myName,
+    age: myAge
+};
+
+let hobbies = ["малювання", "програмування", "письменництво"];
+
+console.log(person);
+console.log(hobbies);
